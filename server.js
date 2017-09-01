3396eaa5bc129da81eb15c13accaa6f7155bdec8
RahulAtDure <rahul@duretechnologies.com>
2017-09-01 15:32:48 +0530

    Basic React Project structure for practise

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 var connect = require("connect");
 var serveStatic = require("serve-static");
 var app = connect();
+var PORT = 3131;
 
 function sayHello(request,response,next){
 	response.setHeader("Content-type","text/plain");
 	response.end("Hello Connect");
 }
 
-app.use(serveStatic(__dirname + "/app")).listen(3031);
+//app.use(serveStatic(__dirname + "/app")).listen(3131);
+
+app.use(serveStatic(__dirname + "/"));
+
+var server = app.listen(PORT, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("React app listening at http://%s:%s", host, port);
+
+});
