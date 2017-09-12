5f912a3c5f95c993e30ef390cfb6bde358a51f4f
rahulshivsharan <rahulshivsharan@gmail.com>
2017-09-12 17:52:47 +0530

    UI re-rendering and managing states (onBlur)

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ app.use(serveStatic(__dirname + "/"));
 
 var server = app.listen(PORT, function () {
 
-  var host = server.address().address
-  var port = server.address().port
+  var host = server.address().address;
+  var port = server.address().port;
 
   console.log("React app listening at http://%s:%s", host, port);
 
